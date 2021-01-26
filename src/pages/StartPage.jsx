303c3809a385084e798830806d6b6195c4f5148c
madeleineenberg <madeleine_enberg@hotmail.com>
2021-01-26 18:37:19 +0100

    style fix

--- src/pages/StartPage.jsx
+++ src/pages/StartPage.jsx
@@ -2,12 +2,11 @@ import React from 'react'
 import InputField from '../components/InputField'
 import JobList from '../components/JobList'
 import Title from '../components/Title'
-import image from '../images/svg.png'
 
 export default function StartPage() {
     return (
         <section>
-            <header style={{ backgroundImage: `url(${image})`}} >
+            <header>
             <Title/>
             <InputField/>
             </header>
