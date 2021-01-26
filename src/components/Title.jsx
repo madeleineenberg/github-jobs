f47c87a3e8a7b6ee486f1c7e7e10bada7cf846e0
madeleineenberg <madeleine_enberg@hotmail.com>
2021-01-26 18:44:54 +0100

    h1 fix

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Title() {
     return (
         <>
-            <h1>GitHub Jobs</h1>
+            <h1 className="title">GitHub Jobs</h1>
             <i className="icon-github-2"/>
         </>
     )
